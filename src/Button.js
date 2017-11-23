e8819b9399ded2058749f43c9868dfcdc6224337
Le Van Hop <av.votinh@gmail.com>
2017-11-23 21:51:06 +0700

    add npm run all

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Button.css'
+
+class Button extends Component {
+    render(){
+        <div className='Button'></div>
+    }
+}
+
+export default Button
